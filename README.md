a5954901e6d3950c6efb9eae2bdf908217aab752
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-11-14 09:57:55 -0500

    task:3.18.0

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.task
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-task/actions/workflows/build.yml/badge.svg)
+
+Installs [task](https://github.com/go-task/task).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.task
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/go-task/task/releases/download
+
+dl() {
+    local app=$1
+    local ver=$2
+    local lchecksums=$3
+    local os=$4
+    local arch=$5
+    local archive_type=${6:-tar.gz}
+    local platform="${os}_${arch}"
+    # https://github.com/go-task/task/releases/download/v3.18.0/task_darwin_amd64.tar.gz
+    local file="${app}_${platform}.${archive_type}"
+    local url="$MIRROR/v$ver/$file"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local app=$1
+    local ver=$2
+    # https://github.com/go-task/task/releases/download/v3.18.0/task_checksums.txt
+    local url="$MIRROR/v$ver/task_checksums.txt"
+    local lchecksums="$DIR/${app}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        curl -sSLf -o $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $app $ver $lchecksums darwin amd64
+    dl $app $ver $lchecksums darwin arm64
+    dl $app $ver $lchecksums linux 386
+    dl $app $ver $lchecksums linux amd64
+    dl $app $ver $lchecksums linux arm64
+}
+
+dl_ver task ${1:-3.18.0}
